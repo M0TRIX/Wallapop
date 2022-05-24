005c70c4b2073c6ac3331cdeb799a088918e5587
Ali Asadi <aliasadi@Alis-MacBook-Pro.local>
2022-05-24 19:41:19 +0430

    init the project

---
+++ HomeTask/HomeTask/LandingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LandingViewController.swift
+//  HomeTask
+//
+//  Created by Ali Asadi on 3/3/1401 AP.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
